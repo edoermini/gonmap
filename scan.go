cc1396d66a48f74b4a4eabb04595cb51a59c98e6
Edoardo Ermini <edoardo@pop-os.localdomain>
2020-07-16 21:43:23 +0200

    added scan struct and functions

--- scan.go
+++ scan.go
@@ -1,36 +1,82 @@
 package gonmap
 
 import (
-	"regexp"
-	"log"
+	"errors"
+	"fmt"
+	"reflect"
 )
 
-type Host string
-
+// Scan contains basic scan information: hosts list and ports list
 type Scan struct {
-	Type string
-	Options string
 	Hosts []Host
+	Ports []int
 }
 
-func HostCheck(h Host) bool {
-	ipRegex := `^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$`
-	domainRegex := `^[a-zA-Z0-9][a-zA-Z0-9-_]{0,61}[a-zA-Z0-9]{0,1}\.([a-zA-Z]{1,6}|[a-zA-Z0-9-]{1,30}\.[a-zA-Z]{2,3})$`
+// AddHost add host h to Scan s
+func (s Scan) AddHost(h Host) (Scan, error) {
 
-	ipMatched, err := regexp.MatchString(ipRegex, string(h))
-	if (err != nil) {
-		log.Fatal(err)
+	if h == Host("") {
+		return s, errors.New("Parameter must be a created Host")
 	}
 
-	domainMatched, err := regexp.MatchString(domainRegex, string(h))
-	if (err != nil) {
-		log.Fatal(err)
+	if !IsHost(h) {
+		return s, errors.New("Parameter must be a valid Host")
+
 	}
 
-	return (ipMatched || domainMatched)
-}
-
-func (s Scan) AddHost(h Host) {
 	s.Hosts = append(s.Hosts, h)
+	return s, nil
 }
 
+// AddPort adds port p to Scan s
+func (s Scan) AddPort(p int) (Scan, error) {
+	if p < 0 || p > 65536 {
+		return s, errors.New("Port parameter must be an integer between 0 and 65536")
+	}
+
+	s.Ports = append(s.Ports, p)
+	return s, nil
+}
+
+// AddPortRange adds ports from min to max
+func (s Scan) AddPortRange(min int, max int) (Scan, error) {
+	if min < 0 || min > 65536 {
+		return s, errors.New("Min parameter must be an integer between 0 and 65536")
+	}
+
+	if max < 0 || max > 65536 {
+		return s, errors.New("Max parameter must be an integer between 0 and 65536")
+	}
+
+	for p := min; p <= max; p++ {
+		s.Ports = append(s.Ports, p)
+	}
+
+	return s, nil
+}
+
+// NewScan return new Scan
+func NewScan(hosts []Host, ports []int) Scan {
+
+	if hosts == nil && ports == nil {
+		return Scan{[]Host{}, []int{}}
+	}
+	if hosts == nil && ports != nil {
+		return Scan{[]Host{}, ports}
+	}
+	if hosts != nil && ports == nil {
+		return Scan{hosts, []int{}}
+	}
+
+	return Scan{hosts, ports}
+
+}
+
+func (s Scan) String() string {
+	return fmt.Sprintf("Scan{%v, %v}", s.Hosts, s.Ports)
+}
+
+// IsEqual cheks if two scans are equal
+func (s Scan) IsEqual(s1 Scan) bool {
+	return reflect.DeepEqual(s.Hosts, s1.Hosts) && reflect.DeepEqual(s.Ports, s1.Ports)
+}
--- scan_test.go
+++ scan_test.go
@@ -4,25 +4,42 @@ import (
 	"testing"
 )
 
-func TestHostCheck(t *testing.T) {
-	var result bool;
-	
-	testSet := map[Host]bool{
-		Host("192.168.1.1"):true,
-		Host("255.255.255.255"):true,
-		Host("888.888.888.888"):false,
-		Host("192.2.3"):false,
-		Host("www.google.com"):true,
-		Host("google.com"):true,
-		Host("google.com.it.it"):false,
-		Host("g"):false,
-		Host("https://www.gloogle.com"):false,
+func TestIsEqual(t *testing.T) {
+	var s1 Scan
+	var s2 Scan
+
+	s1 = Scan{[]Host{Host("192.168.1.1")}, []int{}}
+	s2 = Scan{[]Host{Host("192.168.1.1"), Host("google.com")}, []int{}}
+	s1, _ = s1.AddHost(Host("google.com"))
+
+	if s1.IsEqual(s2) == false {
+		t.Errorf("Test: %s == %s, expected: %t, got: %t", s1, s2, true, false)
 	}
 
-	for test,expected := range testSet {
-		result = HostCheck(test)
-		if (result != expected) {
-			t.Errorf("Test: %s expected %t, got: %t",test, expected, result)
+	s1 = Scan{[]Host{Host("192.168.1.1"), Host("google.com")}, []int{}}
+	s2 = Scan{[]Host{Host("192.168.1.1"), Host("google.com")}, []int{80}}
+
+	if s1.IsEqual(s2) == true {
+		t.Errorf("Test: %s == %s, expected: %t, got: %t", s1, s2, false, true)
+	}
+
+}
+
+func TestAddHost(t *testing.T) {
+	result := NewScan(nil, nil)
+
+	testSet := map[Host]Scan{
+		Host("192.168.1.1"): Scan{[]Host{Host("192.168.1.1")}, []int{}},
+		Host("google.com"):  Scan{[]Host{Host("192.168.1.1"), Host("google.com")}, []int{}},
+	}
+
+	for test, expected := range testSet {
+		result, _ = result.AddHost(test)
+
+		t.Log(result.IsEqual(expected))
+
+		if !result.IsEqual(expected) {
+			t.Errorf("Test: %s, expected: %s, got: %s", test, expected, result)
 		}
 	}
 }
