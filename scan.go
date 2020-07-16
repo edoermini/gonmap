261649346bce5d849414f12347ab04ba934e002b
Edoardo Ermini <edoardo@pop-os.localdomain>
2020-07-16 15:59:20 +0200

    added and tested HostCheck function

---
+++ scan.go
@@ -0,0 +1,36 @@
+package gonmap
+
+import (
+	"regexp"
+	"log"
+)
+
+type Host string
+
+type Scan struct {
+	Type string
+	Options string
+	Hosts []Host
+}
+
+func HostCheck(h Host) bool {
+	ipRegex := `^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`
+	domainRegex := `^[a-zA-Z0-9][a-zA-Z0-9-_]{0,61}[a-zA-Z0-9]{0,1}\.([a-zA-Z]{1,6}|[a-zA-Z0-9-]{1,30}\.[a-zA-Z]{2,3})$`
+
+	ipMatched, err := regexp.MatchString(ipRegex, string(h))
+	if (err != nil) {
+		log.Fatal(err)
+	}
+
+	domainMatched, err := regexp.MatchString(domainRegex, string(h))
+	if (err != nil) {
+		log.Fatal(err)
+	}
+
+	return (ipMatched || domainMatched)
+}
+
+func (s Scan) AddHost(h Host) {
+	s.Hosts = append(s.Hosts, h)
+}
+
---
+++ scan_test.go
@@ -0,0 +1,27 @@
+package gonmap
+
+import (
+	"testing"
+)
+
+func TestHostCheck(t *testing.T) {
+	var result bool;
+	
+	testSet := map[Host]bool{
+		Host("192.168.1.1"):true,
+		Host("255.255.255.255"):true,
+		Host("192.2.3"):false,
+		Host("www.google.com"):true,
+		Host("google.com"):true,
+		Host("google.com.it.it"):false,
+		Host("g"):false,
+		Host("https://www.gloogle.com"):false,
+	}
+
+	for test,expected := range testSet {
+		result = HostCheck(test)
+		if (result != expected) {
+			t.Errorf("Test: %s expected %t, got: %t",test, expected, result)
+		}
+	}
+}
