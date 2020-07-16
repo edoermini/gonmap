e5372eef453c0483221ae0c2eb0cf15a9a71c5e9
MrRadix <edo.ermini@gmail.com>
2020-07-17 00:19:29 +0200

    moved IsHost function here

---
+++ aux.go
@@ -0,0 +1,24 @@
+package gonmap
+
+import (
+	"log"
+	"regexp"
+)
+
+// IsHost returns true if host is a valid host false otherwise
+func IsHost(h string) bool {
+	ipRegex := `^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$`
+	domainRegex := `^[a-zA-Z0-9][a-zA-Z0-9-_]{0,61}[a-zA-Z0-9]{0,1}\.([a-zA-Z]{1,6}|[a-zA-Z0-9-]{1,30}\.[a-zA-Z]{2,3})$`
+
+	ipMatched, err := regexp.MatchString(ipRegex, h)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	domainMatched, err := regexp.MatchString(domainRegex, h)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return (ipMatched || domainMatched)
+}
