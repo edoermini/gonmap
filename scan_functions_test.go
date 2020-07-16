538803e3a25fb52bb01734dfc9620687ca24f834
MrRadix <edo.ermini@gmail.com>
2020-07-17 00:20:04 +0200

    started write scan functions

---
+++ scan_functions_test.go
@@ -0,0 +1,16 @@
+package gonmap
+
+import "testing"
+
+func TestTCPScan(t *testing.T) {
+	scan, _ := NewScan(nil, nil, 5)
+	scan, _ = scan.AddHost("google.com")
+	scan, _ = scan.AddPortRange(22, 80)
+
+	out, err := scan.TCPScan()
+	if err != nil {
+		t.Log(err)
+	} else {
+		t.Log(out)
+	}
+}
