87523328fbbe28cce0f48dc61279ff450979edad
MrRadix <edo.ermini@gmail.com>
2020-07-17 22:24:11 +0200

    finished writing scan types

--- functions.go
+++ functions.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os/exec"
 	"strings"
 )
@@ -13,17 +14,18 @@ type settings struct {
 	portsFlag   string
 	hosts       string
 	performance string
+	versionScan string
 }
 
 func arrayToString(a []int, delim string) string {
 	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
 }
 
-func getSettings(scanFlag string, s Scan) settings {
+func getSettings(s Scan) settings {
 	configuration := settings{
-		scanFlag:  scanFlag,
-		portsFlag: "-p" + arrayToString(s.GetPorts(), ","),
-		hosts:     strings.Join(s.GetHosts(), " "),
+		portsFlag:   "-p" + arrayToString(s.GetPorts(), ","),
+		hosts:       strings.Join(s.GetHosts(), " "),
+		versionScan: "",
 	}
 
 	switch s.performance {
@@ -39,16 +41,16 @@ func getSettings(scanFlag string, s Scan) settings {
 		configuration.performance = "-T4"
 	case 5:
 		configuration.performance = "-T5"
+	}
 
+	if s.versionScan {
+		configuration.versionScan = "-sV"
 	}
 
 	return configuration
 }
 
-// TCPScan makes a tcp scan; command: nmap -sT ...
-func (s Scan) TCPScan() ([]byte, error) {
-
-	config := getSettings("-sT", s)
+func runScan(config settings) ([]byte, error) {
 
 	// Finds nmap binary path
 	nmap, err := exec.LookPath("nmap")
@@ -56,8 +58,11 @@ func (s Scan) TCPScan() ([]byte, error) {
 		return nil, err
 	}
 
-	cmd := exec.Command(nmap, "-oX", "-", "-vvv", config.performance, config.scanFlag, config.portsFlag, config.hosts)
-	//fmt.Println(cmd)
+	if config.hosts == "" {
+		return nil, errors.New("Must be present at least one host")
+	}
+
+	cmd := exec.Command(nmap, "-oX", "-", "-vvvvv", config.performance, config.scanFlag, config.portsFlag, config.versionScan, config.hosts)
 
 	// Configure output pipes
 	errPipe, err := cmd.StderrPipe()
@@ -90,16 +95,147 @@ func (s Scan) TCPScan() ([]byte, error) {
 		return nil, errors.New(err.Error() + "\n" + string(stderr))
 	}
 
-	fmt.Println(string(stdout))
+	return stdout, nil
+}
 
-	return nil, nil
+// TCPScan makes a tcp scan; command: nmap -sT ...
+func (s Scan) TCPScan() (map[string]interface{}, error) {
+
+	config := getSettings(s)
+	config.scanFlag = "-sT"
+
+	xml, err := runScan(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return NmapXMLParse(xml), nil
 
 }
-func UDPScan()  {}
-func SYNScan()  {}
-func ACKScan()  {}
-func FINScan()  {}
-func NULLScan() {}
-func XMASScan() {}
-func RPCScan()  {}
-func IDLEScan() {}
+
+func (s Scan) UDPScan() (map[string]interface{}, error) {
+	config := getSettings(s)
+	config.scanFlag = "-sU"
+
+	xml, err := runScan(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return NmapXMLParse(xml), nil
+}
+
+func (s Scan) SYNScan() (map[string]interface{}, error) {
+	config := getSettings(s)
+	config.scanFlag = "-sS"
+
+	xml, err := runScan(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return NmapXMLParse(xml), nil
+
+}
+
+func (s Scan) ACKScan() (map[string]interface{}, error) {
+	config := getSettings(s)
+	config.scanFlag = "-sA"
+
+	xml, err := runScan(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return NmapXMLParse(xml), nil
+}
+
+func (s Scan) FINScan() (map[string]interface{}, error) {
+	config := getSettings(s)
+	config.scanFlag = "-sF"
+
+	xml, err := runScan(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return NmapXMLParse(xml), nil
+}
+
+func (s Scan) NULLScan() (map[string]interface{}, error) {
+	config := getSettings(s)
+	config.scanFlag = "-sN"
+
+	xml, err := runScan(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return NmapXMLParse(xml), nil
+}
+
+func (s Scan) XMASScan() (map[string]interface{}, error) {
+	config := getSettings(s)
+	config.scanFlag = "-sX"
+
+	xml, err := runScan(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return NmapXMLParse(xml), nil
+}
+
+func (s Scan) IDLEScan(zombie string) (map[string]interface{}, error) {
+	if !IsHost(zombie) {
+		return nil, errors.New("Zombie target must be a valid host")
+	}
+
+	config := getSettings(s)
+	config.scanFlag = "-sI"
+
+	// Finds nmap binary path
+	nmap, err := exec.LookPath("nmap")
+	if err != nil {
+		return nil, err
+	}
+
+	if config.hosts == "" {
+		return nil, errors.New("Must be present at least one host")
+	}
+
+	cmd := exec.Command(nmap, "-oX", "-", "-vvvvv", config.performance, config.scanFlag, zombie, config.portsFlag, config.versionScan, config.hosts)
+
+	// Configure output pipes
+	errPipe, err := cmd.StderrPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	outPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	// Start command
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	stdout, err := ioutil.ReadAll(outPipe)
+	if err != nil {
+		return nil, err
+	}
+
+	stderr, err := ioutil.ReadAll(errPipe)
+	if err != nil {
+		return nil, err
+	}
+
+	// Waits nmap command
+	if err := cmd.Wait(); err != nil {
+		return nil, errors.New(err.Error() + "\n" + string(stderr))
+	}
+
+	return NmapXMLParse(stdout), nil
+} // Without examples
--- functions_test.go
+++ functions_test.go
@@ -1,18 +1,104 @@
 package gonmap
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestTCPScan(t *testing.T) {
-	scan, _ := NewScan(nil, nil, 5)
+func ExampleTCPScan() {
+	scan := NewInitScan()
 	scan, _ = scan.AddHost("google.com")
-	scan, _ = scan.AddPortRange(22, 80)
+	scan, _ = scan.AddPort(80)
+	scan, _ = scan.SetPerformance(5)
+	scan = scan.SetVersionScan(false)
 
 	out, err := scan.TCPScan()
 	if err != nil {
-		t.Log(err)
-	} else {
-		t.Log(out)
+		return
 	}
+	fmt.Println(out)
+}
+
+func ExampleUDPScan(t *testing.T) {
+	scan := NewInitScan()
+	scan, _ = scan.AddHost("google.com")
+	scan, _ = scan.AddPort(80)
+	scan, _ = scan.SetPerformance(5)
+	scan = scan.SetVersionScan(false)
+
+	out, err := scan.UDPScan()
+	if err != nil {
+		return
+	}
+	fmt.Println(out)
+}
+
+func ExampleSYNScan(t *testing.T) {
+	scan := NewInitScan()
+	scan, _ = scan.AddHost("google.com")
+	scan, _ = scan.AddPort(80)
+	scan, _ = scan.SetPerformance(5)
+	scan = scan.SetVersionScan(false)
+
+	out, err := scan.SYNScan()
+	if err != nil {
+		return
+	}
+	fmt.Println(out)
+}
+
+func ExampleACKScan(t *testing.T) {
+	scan := NewInitScan()
+	scan, _ = scan.AddHost("google.com")
+	scan, _ = scan.AddPort(80)
+	scan, _ = scan.SetPerformance(5)
+	scan = scan.SetVersionScan(false)
+
+	out, err := scan.ACKScan()
+	if err != nil {
+		return
+	}
+	fmt.Println(out)
+}
+
+func ExampleFINScan(t *testing.T) {
+	scan := NewInitScan()
+	scan, _ = scan.AddHost("google.com")
+	scan, _ = scan.AddPort(80)
+	scan, _ = scan.SetPerformance(5)
+	scan = scan.SetVersionScan(false)
+
+	out, err := scan.FINScan()
+	if err != nil {
+		return
+	}
+	fmt.Println(out)
+}
+
+func ExampleNULLScan() {
+	scan := NewInitScan()
+	scan, _ = scan.AddHost("google.com")
+	scan, _ = scan.AddPort(80)
+	scan, _ = scan.SetPerformance(5)
+	scan = scan.SetVersionScan(false)
+
+	out, err := scan.NULLScan()
+	if err != nil {
+		return
+	}
+	fmt.Println(out)
+}
+
+func ExampleXMASScan(t *testing.T) {
+	scan := NewInitScan()
+	scan, _ = scan.AddHost("google.com")
+	scan, _ = scan.AddPort(80)
+	scan, _ = scan.SetPerformance(5)
+	scan = scan.SetVersionScan(false)
+
+	out, err := scan.XMASScan()
+	if err != nil {
+		return
+	}
+	fmt.Println(out)
 }
